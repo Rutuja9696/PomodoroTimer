2f11f350191e78c968ce90e7b796a024f4f1605e
Rutuja9696 <rutuja.patil@contentstact.com>
2020-11-27 00:12:31 +0530

    created basic timer

---
+++ src/main.js
@@ -0,0 +1,23 @@
+var minutes = 25;
+var seconds = 00;
+
+function timer() {
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
+function start() {
+  minutes = 24;
+  seconds = 59;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  var minInt = setInterval(minutesTimer, 60000);
+  var secInt = setInterval(secondsTimer, 1000);
+  function minutesTimer() {
+    minutes = minutes - 1;
+    document.getElementById("minutes").innerHTML = minutes;
+  }
+  function secondsTimer() {
+    seconds = seconds - 1;
+    document.getElementById("seconds").innerHTML = seconds;
+  }
+}
